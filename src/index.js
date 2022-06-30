f996f5634963b266e9a6ef4b5871ed5b1cb2fe8d
Kimginam97 <rlarlska79@naver.com>
2022-06-30 16:22:50 +0900

    3D 상자, 원기둥 만들기

--- src/index.js
+++ src/index.js
@@ -1,24 +1,85 @@
-import { Ion, Viewer, createWorldTerrain, createOsmBuildings, Cartesian3, Math } from "cesium";
-import "cesium/Widgets/widgets.css";
-import "../src/css/main.css"
+import * as Cesium from 'cesium';
+import 'cesium/Widgets/widgets.css';
+import '../src/css/main.css';
 
 // Your access token can be found at: https://cesium.com/ion/tokens.
 // This is the default access token
-Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlYWE1OWUxNy1mMWZiLTQzYjYtYTQ0OS1kMWFjYmFkNjc5YzciLCJpZCI6NTc3MzMsImlhdCI6MTYyNzg0NTE4Mn0.XcKpgANiY19MC4bdFUXMVEBToBmqS8kuYpUlxJHYZxk';
+Cesium.Ion.defaultAccessToken =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJlYWE1OWUxNy1mMWZiLTQzYjYtYTQ0OS1kMWFjYmFkNjc5YzciLCJpZCI6NTc3MzMsImlhdCI6MTYyNzg0NTE4Mn0.XcKpgANiY19MC4bdFUXMVEBToBmqS8kuYpUlxJHYZxk';
 
 // Initialize the Cesium Viewer in the HTML element with the `cesiumContainer` ID.
-const viewer = new Viewer('cesiumContainer', {
-  terrainProvider: createWorldTerrain()
+const viewer = new Cesium.Viewer('cesiumContainer', {
+  terrainProvider: Cesium.createWorldTerrain(),
+  animation: false,
+  baseLayerPicker: false,
+  fullscreenButton: false,
+  // infoBox: false,
+  // geocoder: false,
+  // homeButton: false,
+  sceneModePicker: false,
+  navigationHelpButton: false,
+  timeline: false,
 });
 
-// Add Cesium OSM Buildings, a global 3D buildings layer.
-viewer.scene.primitives.add(createOsmBuildings());   
+// 밑에 로고 삭제
+viewer.bottomContainer.style.display = 'none';
 
-// Fly the camera to San Francisco at the given longitude, latitude, and height.
+// viewer.dataSources.add(
+//   Cesium.GeoJsonDataSource.load(
+//     'http://localhost:8080/geoserver/SeoulCity/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=SeoulCity%3AF_FAC_BUILDING_11_202206&maxFeatures=50&outputFormat=application%2Fjson&srsname=EPSG:4326',
+//     {
+//       fill: Cesium.Color.PINK,
+//     }
+//   )
+// );
+
+// viewer.dataSources.add(
+//   Cesium.GeoJsonDataSource.load(
+//     'http://localhost:8080/geoserver/Administrative_district/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=Administrative_district%3AZ_NGII_N3A_G0100000&maxFeatures=50&outputFormat=application%2Fjson&srsname=EPSG:4326'
+//   )
+// );
+
+// 서울 위치로 카메라 이동
 viewer.camera.flyTo({
-  destination : Cartesian3.fromDegrees(-122.4175, 37.655, 400),
-  orientation : {
-    heading : Math.toRadians(0.0),
-    pitch : Math.toRadians(-15.0),
-  }
+  destination: Cesium.Cartesian3.fromDegrees(126.975, 37.4575, 700),
+  orientation: {
+    heading: Cesium.Math.toRadians(0.0),
+    pitch: Cesium.Math.toRadians(-15.0),
+  },
+});
+
+const makeBox = document.querySelector('#boxbutton');
+
+makeBox.addEventListener('click', () => {
+  const box = viewer.entities.add({
+    name: 'Box',
+    position: Cesium.Cartesian3.fromDegrees(126.924, 37.524, 255.0),
+    box: {
+      dimensions: new Cesium.Cartesian3(500.0, 500.0, 500.0),
+      material: Cesium.Color.WHITE,
+      outline: false,
+      outlineColor: Cesium.Color.BLACK,
+    },
+  });
+  viewer.zoomTo(viewer.entities);
+  alert('3D 상자를 생성합니다.');
+});
+
+const makeCylinder = document.querySelector('#cylinderbutton');
+
+makeCylinder.addEventListener('click', () => {
+  const cylinder = viewer.entities.add({
+    name: 'Cylinder',
+    position: Cesium.Cartesian3.fromDegrees(126.907, 37.527, 255.0),
+    cylinder: {
+      length: 490,
+      topRadius: 200,
+      bottomRadius: 200,
+      material: Cesium.Color.WHITE,
+      outline: false,
+      outlineColor: Cesium.Color.BLACK,
+    },
+  });
+  viewer.zoomTo(viewer.entities);
+  alert('3D 상자를 생성합니다.');
 });
